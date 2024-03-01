227db41923f8851d4c81960b56245423349f7263
Murilo Luiz Jaboinski <143234115+muriloLuix@users.noreply.github.com>
2024-03-01 15:02:20 -0300

    Adding dark theme

--- assets/js/index.js
+++ assets/js/index.js
@@ -91,3 +91,18 @@ function handleBoardClick(ev) {
 }
 // Adiciona o evento no botão que inicia o jogo
 document.getElementById('start').addEventListener('click', initializeGame)
+
+
+document.getElementById('themeSwitcher').addEventListener('click', function(){
+  if (main.dataset.theme === 'dark'){
+    root.style.setProperty('--bg-color', '#f1f5f9')
+    root.style.setProperty('--font-color', '#212529')
+    root.style.setProperty('--primary-color', '#26834a')
+    main.dataset.theme = 'light'
+  } else {
+    root.style.setProperty('--bg-color', '#212529')
+    root.style.setProperty('--font-color', '#f1f5f9')
+    root.style.setProperty('--primary-color', '#4dff91')
+    main.dataset.theme = 'dark'
+  }
+})
